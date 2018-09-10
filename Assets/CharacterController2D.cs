3a4d980daab71cbbd545409f8abe95b08352f5cf
Johanna G <johanna.granstrom@gmail.com>
2018-09-10 14:56:25 +0000

    Added player and enemy following the player

---
+++ Assets/CharacterController2D.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour {
+
+	//Initialization
+	public float movementSpeed; //Players movement speed
+	private Rigidbody2D rb2d; 
+	private Vector2 moveVelocity;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float moveHorizontal = Input.GetAxisRaw("Horizontal"); //Genom att använda GetAxisRaW får man en mer robotic movement, ingen acceleration
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+
+		//Use the two floats to create a new Vector2 variable movement
+		Vector2 movementInput = new Vector2(moveHorizontal, moveVertical);
+
+		//Call the AddForce function of our rigid body supplying movement multiplied by speed to move our player
+		//rb2d.AddForce (movementInput*movementSpeed);
+		//kommentera bort
+		moveVelocity = movementInput.normalized*movementSpeed;
+		
+	}
+
+	//Alternativt ifall vi vill att karaktären ska vara kinetic
+	void FixedUpdate()
+	{
+		rb2d.MovePosition (rb2d.position + moveVelocity * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float movementSpeed;
+
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.MoveTowards (transform.position, player.position, movementSpeed*Time.deltaTime);
+	}
+}
