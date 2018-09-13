e3d48f599ecd595b0fd69ce23ea0b0e15ba4afe0
Johanna G <johanna.granstrom@gmail.com>
2018-09-13 11:16:38 +0000

    Added enemies spawning random at specific spawning points

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] enemyPattern;
+
+	private float timeBtwSpawn;
+	public float startTimeBtwSpawn;
+	public float decreaseTime;
+	public float minTime = 0.65f;
+	
+	// Update is called once per frame
+	private void Update () {
+		if (timeBtwSpawn <= 0) {
+			
+			int rand = Random.Range(0, enemyPattern.Length);
+			Instantiate(enemyPattern[rand], transform.position, Quaternion.identity);
+			timeBtwSpawn = startTimeBtwSpawn; //Wait x amount of seconds before another enemy spawn in game
+
+			if(startTimeBtwSpawn > minTime){
+				startTimeBtwSpawn -= decreaseTime;//Time between spawns will be a little bit smaller next time an enemy spawns
+			}
+		} 
+		else {
+			timeBtwSpawn -= Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Scripts/spawnPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPoint : MonoBehaviour {
+
+	public GameObject Enemy;
+	
+	// Update is called once per frame
+	void Start () {
+		Instantiate(Enemy, transform.position, Quaternion.identity);
+	}
+}
