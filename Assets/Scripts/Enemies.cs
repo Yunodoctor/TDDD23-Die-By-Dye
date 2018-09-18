0820b11743c2f853ae41f49dadcff4690751d51d
Johanna G <johanna.granstrom@gmail.com>
2018-09-18 08:52:13 +0000

    Enemy not colliding

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -8,6 +8,7 @@ public class Enemies : MonoBehaviour {
 	public int health; //Enemy total health
     private Transform target; //Holds the gameObject the enemies are chasing
     private Player player;
+	public float stopDistance;
 
     //Drop
     public bool HealthDrop;
@@ -25,8 +26,9 @@ public class Enemies : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//Enemy moving towards player
-        transform.position = Vector2.MoveTowards(transform.position, target.position, movementSpeed*Time.deltaTime); //Move the enemies position towards the player position at a certain speed (from, to, speed). Time.deltaTime makes sure that the enemies won't run faster on a fast computer comparing to a slow
-
+		if(Vector2.Distance(transform.position, target.position) > stopDistance){
+        	transform.position = Vector2.MoveTowards(transform.position, target.position, movementSpeed*Time.deltaTime); //Move the enemies position towards the player position at a certain speed (from, to, speed). Time.deltaTime makes sure that the enemies won't run faster on a fast computer comparing to a slow
+		}
 		//If the enemy takes damage, destroy object
 		if(health <= 0){
 			//Instantiate (deathEffect, transform.position, Quaternion.identity); //Ifall vi vill ha en cool effekt när fienden dör
--- Assets/Scripts/spawnPoint.cs
+++ Assets/Scripts/spawnPoint.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class spawnPoint : MonoBehaviour {
 
 	public GameObject Enemy;
-	
+	//Vector2 whereToSpawn; 
+	//float randPos;
+
 	// Update is called once per frame
 	void Start () {
+		//randPos = Random.Range (-2f, -2f);
+		//whereToSpawn = new Vector2 (randPos, transform.position.y);
 		Instantiate(Enemy, transform.position, Quaternion.identity);
 	}
 }
