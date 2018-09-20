1aa47ae0372b6c9d24a8adae0e3f8a4f700c7d72
Johanna G <johanna.granstrom@gmail.com>
2018-09-20 09:41:39 +0000

    Spawner script updated so enemies spawn at random positions. EnemyPattern updated with several spawnpoitns.

--- Assets/Scripts/spawnPoint.cs
+++ Assets/Scripts/spawnPoint.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class spawnPoint : MonoBehaviour {
 
 	public GameObject Enemy;
-	//Vector2 whereToSpawn; 
-	//float randPos;
+	Vector2 whereToSpawn; 
+	float randPos;
 
 	// Update is called once per frame
 	void Start () {
-		//randPos = Random.Range (-2f, -2f);
-		//whereToSpawn = new Vector2 (randPos, transform.position.y);
-		Instantiate(Enemy, transform.position, Quaternion.identity);
+		randPos = Random.Range (-10.0f, 10.0f);
+		whereToSpawn = new Vector2 (randPos, transform.position.y);
+		Instantiate (Enemy, whereToSpawn, Quaternion.identity);
+		//Instantiate(Enemy, transform.position, Quaternion.identity);
 	}
 }
