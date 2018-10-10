5c5ac993bf1da93d0a92b88c771179e80a16df5c
Emilie Ho <emeliewho@gmail.com>
2018-10-10 13:48:16 +0000

    No Comments

--- Assets/Scripts/CompKeyboard.cs
+++ Assets/Scripts/CompKeyboard.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Keyboard : MonoBehaviour
+public class CompKeyboard : MonoBehaviour
 {
     //Sprites
     public Image Keyboards;
