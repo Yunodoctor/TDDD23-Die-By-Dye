b8b7703113894fa4efb6d4dd40dead00a20aa775
Emilie Ho <emeliewho@gmail.com>
2018-10-14 15:45:51 +0000

    Fixed so enemies can move past trail

--- Assets/Scripts/PowerDownSlow.cs
+++ Assets/Scripts/PowerDownSlow.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class PowerDownSlow : MonoBehaviour
 {
-
     private Player player;
 
-
-    // Use this for initialization
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 
-    // Update is called once per fwWrame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.CompareTag("Player"))
+        {
+            player.movementSpeed = 1f;
+        }
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            player.movementSpeed = 3f;
+            player.movementSpeed = 5f;
         }
     }
 }
