264f782ac4c409f2080db06164b4f973fcf7287b
Emilie Ho <emeliewho@gmail.com>
2018-10-14 11:28:38 +0000

    No Comments

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class StartMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
 
 	public void PlayGame () {
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);		
 	}
+
+    public void SetVolume (float Volume)
+    {
+        Debug.Log(Volume);
+        audioMixer.SetFloat("Volume", Volume);
+    }
 
     public void QuitGame()
     {
